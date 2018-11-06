d74edcaa2c0f4efb1bf7677a4d2cc37481d75ed5
Kevin Bates <kevin.t.bates@centene.com>
2018-11-06 10:42:41 -0600

    Change path for import

--- logstash_exporter.go
+++ logstash_exporter.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"github.com/BonnierNews/logstash_exporter/collector"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/common/log"
-	"github.com/prometheus/common/version"
-	"gopkg.in/alecthomas/kingpin.v2"
 	"net/http"
 	_ "net/http/pprof"
 	"sync"
 	"time"
+
+	"github.com/detosu/logstash_exporter/collector"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/version"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
